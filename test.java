e3bc72776b328321fd8606348599c516f27d8ba8
상우진 (Sang Woo Jin) <80154292+SangWJDev@users.noreply.github.com>
2025-10-16 15:50:20 +0900

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! 👋");
         System.out.println("I'm Woojin!");
-        System.out.println("Once upon a time");
+     
     }
 }
