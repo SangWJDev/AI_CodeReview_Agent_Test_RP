38fd2a5ed2e9ec557a162944883e1b35c0cc4675
상우진 (Sang Woo Jin) <80154292+SangWJDev@users.noreply.github.com>
2025-10-16 16:00:22 +0900

    Remove duplicate print statements in main
    Removed duplicate print statements in main method.

--- test.java
+++ test.java
@@ -3,6 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! 👋");
         System.out.println("I'm Woojin!");
-     
+        System.out.println("Hello, World! 👋");
+        System.out.println("I'm Woojin!");
+        
     }
 }
