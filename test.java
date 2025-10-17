357af2a77cd7fc18df758ee4ba36bf9eff0a18a9
상우진 (Sang Woo Jin) <80154292+SangWJDev@users.noreply.github.com>
2025-10-17 16:38:06 +0900

    Remove duplicate print statement in test.java

--- test.java
+++ test.java
@@ -5,6 +5,6 @@ public class HelloWorld {
         System.out.println("I'm Woojin!");
         System.out.println("Hello, World! 👋");
         System.out.println("I'm Woojin!");
-        
+            System.out.println("I'm Woojin!");
     }
 }
